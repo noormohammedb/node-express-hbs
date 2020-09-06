899fcd5f38e8970240491431703bb20d90b0aa85
noormohammedb <nooru.mohad@gmail.com>
2020-09-06 15:16:34 +0530

    if-else in hbs

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', (req,res,next) => {
   let hbsObject = {items: ['apple','mango']};
-  let comment = {comment1: {comment_data: 'this is comment', comment_date: '02-05-2020'}};
+  let comment = {comment1: {comment_data: 'hbs is very flexible', comment_date: '02-05-2020'}};
+  let admin = {heading: 'if-else in hbs', auth: true};
 
-  res.render('index', {hbsObject, comment, heading: 'this is heading', title: 'index-page'});
+  res.render('index', {hbsObject, comment, heading: 'loop in hbs', title: 'index-page', admin});
 });
 
 module.exports = router;
